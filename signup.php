ea1d93306f889996872670ca245607bc25028c86
zepedro <1231677@isep.ipp.pt>
2025-06-06 16:24:06 +0100

    teste8php

--- signup.php
+++ signup.php
@@ -1,19 +1,28 @@
 <?php
-// Criar a base de dados e a tabela (só na primeira vez)
-$db = new SQLite3('database.db');
-$db->exec("CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL,
-    last_access TEXT
-)");
+$db = new SQLite3('registoutilizadores.db');
 
-// Se vier dados do formulário (POST), tenta registar
+ // criação da tabela utilizadores
+ $db->exec("CREATE TABLE utilizadores(id INTEGER PRIMARY KEY, email TEXT, password TEXT)"); //(1)
+ $db->exec("INSERT INTO utilizadores(email, password) VALUES('user1@example.com', 'password1')"); //(2)
+ echo "<h3>Tabela de utilizadores </h3>";
+ $sqlvar = "select * from utilizadores ;";
+
+ $result = $db->query($sqlvar); //(3)
+ echo “<table>\n<th> Id </th><th> email </th><th> password </th>\n”;
+ while ($row = $result->fetchArray(SQLITE3_ASSOC)) //(4)
+ {
+ echo ‘<tr><td>’ . $row['id'] . '</td><td>' . $row['email'] . '</td><td>' . $row['password']
+ . “</td></tr>\n”;
+ }
+ echo ‘</table>’;
+ unset($db);
+
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $stmt = $db->prepare("INSERT INTO users (email, password) VALUES (:email, :password)");
+    $stmt = $db->prepare("INSERT INTO utilizadores (email, password) VALUES (:email, :password)");
     $stmt->bindValue(':email', $email, SQLITE3_TEXT);
     $stmt->bindValue(':password', $password, SQLITE3_TEXT);
 
